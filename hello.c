0c15f4b75f909b37cc086c9c546c26e771165ba7
Tommy Tran <ttran43@huskers.unl.edu>
2021-08-30 00:15:20 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Truong Tran
+ *
+ * This file prints my major
+ *
+ *
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    printf("Truong Tommy Tran!\n");
+    printf("Your major is Electrical Engineering!\n");
+
+
+    return 0;
+}
